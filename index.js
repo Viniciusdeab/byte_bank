38afc42b0357c75f3914f4d666d4c002c898d0b1
Vinicius <viniciusdeab@outlook.com>
2022-04-01 20:33:54 -0300

    Teste de execução para verificação da senha de funcionarios e clientes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import {Cliente} from "./conta/Cliente.js"
+import {Cliente} from "./Cliente.js"
 import { Gerente } from "./funcionarios/Gerente.js"
 import { Diretor } from "./funcionarios/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000,  12345678900);
+diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Ricardor", 5000, 12378945601);
+gerente.cadastrarSenha("1234");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const cliente = new Cliente("Lais", 78945612379, "4567")
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+console.log(gerenteEstaLogado, diretorEstaLogado);
 
-console.log(estaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "4567");
+console.log(clienteEstaLogado);
